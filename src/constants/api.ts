e663263242d5ec4054bb850cba73db3f28dedc1e
ArijitPatra2906 <patraarijit440@gmail.com>
2024-01-30 17:08:28 +0530

    base url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,2 +1,2 @@
-export const API_DOMAIN = "https://salesb2b.trainright.fit/";
-// export const API_DOMAIN = "https://salesb2c.trainright.fit/";
+// export const API_DOMAIN = "https://salesb2b.trainright.fit/";
+export const API_DOMAIN = "https://salesb2c.trainright.fit/";
