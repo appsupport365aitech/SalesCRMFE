c70cf978f61103d36691c95f4aa625543f4e9c56
ArijitPatra2906 <patraarijit440@gmail.com>
2024-01-30 18:57:37 +0530

    merge

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,2 +1,5 @@
+///////////// ///////////// dev ///////////// /////////////
 // export const API_DOMAIN = "https://salesb2b.trainright.fit/";
+
+/////////// /////////// production /////////////  ///////////
 export const API_DOMAIN = "https://salesb2c.trainright.fit/";
